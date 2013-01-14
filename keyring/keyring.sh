d3aea54719cffd01172b8486340bfc515dd10698
Luca Falavigna <dktrkranz@debian.org>
2013-01-14 22:14:57 +0100

    refresh command added

--- keyring/keyring.sh
+++ keyring/keyring.sh
@@ -6,6 +6,8 @@ elif [ "$1" == "delete" ]; then
 	gpg --no-default-keyring --keyring $PWD/debomatic.gpg --keyserver pgp.mit.edu --delete-key $2
 elif [ "$1" == "list" ]; then
 	gpg --no-default-keyring --keyring $PWD/debomatic.gpg --keyserver pgp.mit.edu --list-keys
+elif [ "$1" == "refresh" ]; then
+	gpg --no-default-keyring --keyring $PWD/debomatic.gpg --keyserver pgp.mit.edu --refresh-keys
 else
 	echo "Unknown command $1"
 fi
