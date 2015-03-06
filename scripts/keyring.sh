2cb1924e21b403a2e095c3e3df9a1fadf0617df7
Luca Falavigna <dktrkranz@debian.org>
2015-03-06 10:48:26 +0100

    Reorganize configuration files

---
+++ scripts/keyring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" == "add" ]; then
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --recv-key $2
+elif [ "$1" == "delete" ]; then
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --delete-key $2
+elif [ "$1" == "list" ]; then
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --list-keys
+elif [ "$1" == "refresh" ]; then
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --refresh-keys
+else
+	echo "Unknown command $1"
+fi
