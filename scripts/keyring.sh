5a347ffbcfdb2aa6aca225a96f62199f7a100fb4
Luca Falavigna <dktrkranz@debian.org>
2015-08-28 13:56:13 +0200

    Allow importing of GPG keys stored on local hard disk

--- scripts/keyring.sh
+++ scripts/keyring.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == "add" ]; then
 	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --recv-key $2
+elif [ "$1" == "import" ]; then
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --import $2
 elif [ "$1" == "delete" ]; then
 	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --delete-key $2
 elif [ "$1" == "list" ]; then
