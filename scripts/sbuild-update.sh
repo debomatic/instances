e0d6a9f058e60c62e788917227244b457bbcca71
Luca Falavigna <dktrkranz@debian.org>
2019-07-03 07:19:48 +0200

    Remove useless scripts, now merged in debomatic itself

--- scripts/sbuild-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for chroot in `schroot -l | perl -ne '/chroot:(\S+-debomatic)/ && print "$1\n"'`
-do
-        [[ $chroot =~ [^-]+-([^-]+)-debomatic ]]
-        arch=${BASH_REMATCH[1]}
-        sbuild-update -udcar --arch=$arch $chroot
-done
