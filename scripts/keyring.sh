483447bf46843ca29aa740c9a4f1e84c73cf7737
Luca Falavigna <dktrkranz@debian.org>
2020-10-07 07:10:05 +0200

    Change GPG server

--- scripts/keyring.sh
+++ scripts/keyring.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$1" == "add" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --recv-key $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --recv-key $2
 elif [ "$1" == "import" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --import $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --import $2
 elif [ "$1" == "delete" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --delete-key $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --delete-key $2
 elif [ "$1" == "list" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --list-keys
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --list-keys
 elif [ "$1" == "refresh" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver pgp.mit.edu --refresh-keys
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --refresh-keys
 else
 	echo "Unknown command $1"
 fi
