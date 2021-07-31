b4196e0de970d86cc0b408e2a7d5b49afd16869d
Luca Falavigna <dktrkranz@debian.org>
2021-07-31 11:25:57 +0200

    Change PGP server

--- scripts/keyring.sh
+++ scripts/keyring.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$1" == "add" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --recv-key $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.openpgp.org --recv-key $2
 elif [ "$1" == "import" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --import $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.openpgp.org --import $2
 elif [ "$1" == "delete" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --delete-key $2
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.openpgp.org --delete-key $2
 elif [ "$1" == "list" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --list-keys
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.openpgp.org --list-keys
 elif [ "$1" == "refresh" ]; then
-	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.gnupg.net --refresh-keys
+	gpg --no-default-keyring --keyring $PWD/../keyring/debomatic.gpg --keyserver keys.openpgp.org --refresh-keys
 else
 	echo "Unknown command $1"
 fi
